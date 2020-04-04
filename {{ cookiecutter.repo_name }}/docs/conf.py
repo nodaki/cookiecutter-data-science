e2ba494761c817202605cb8f1b064b114665a87b
akihiro noda <aki.ap11@gmail.com>
2020-04-04 18:35:02 +0900

    feat: Change settings for sphinx-apidoc and change theme of Sphinx document.

--- {{ cookiecutter.repo_name }}/docs/conf.py
+++ {{ cookiecutter.repo_name }}/docs/conf.py
@@ -14,6 +14,8 @@
 import os
 import sys
 
+sys.path.insert(0, os.path.abspath(".."))
+
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
@@ -26,7 +28,7 @@ import sys
 
 # Add any Sphinx extension module names here, as strings. They can be extensions
 # coming with Sphinx (named 'sphinx.ext.*') or your custom ones.
-extensions = []
+extensions = ['sphinx.ext.autodoc', 'sphinx.ext.napoleon', 'sphinx_click.ext']
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
@@ -91,7 +93,7 @@ pygments_style = 'sphinx'
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
-html_theme = 'default'
+html_theme = 'sphinx_rtd_theme'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
